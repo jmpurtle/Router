e85fe55ad11d9cfc98a35ab4db7030e7b39a2b3f
John Purtle <purtlejohnmark@gmail.com>
2016-04-10 14:40:40 -0500

    Updated Basic example with templating
    All requests currently goes to noResource.phtml
    Using PHPTenjin as templating engine.

--- examples/Basic/index.php
+++ examples/Basic/index.php
@@ -5,27 +5,46 @@ namespace Application {
 	require_once 'autoload.php';
 
 	putenv('APP_ROLE=PRODUCTION');
-	$logger = new \Loggers\ScreenLogger();
-	$router = new \Routers\ObjectRouter($logger);
+	$logger   = new \Loggers\ScreenLogger();
+	$router   = new \Routers\ObjectRouter($logger);
 	$dispatch = new \Dispatchers\SimpleDispatcher();
+	$context  = null;
 
-	//Request parsing
-	$request = $_SERVER['REQUEST_URI'];
-	$webRoot = str_replace($_SERVER['DOCUMENT_ROOT'], '', __DIR__);
-	$request = explode('/', str_replace($webRoot, '', $request));
-	$request = array_values(array_filter($request));
+	//Request parsing, assuming we won't have to deal with unix paths or non-standard paths like 'people.27.foo'
+	$request   = $_SERVER['REQUEST_URI'];
+	$webRoot   = str_replace($_SERVER['DOCUMENT_ROOT'], '', __DIR__);
+	$request   = explode('/', str_replace($webRoot, '', $request));
+	$request   = array_values(array_filter($request));
+	$assetRoot = $webRoot . '/assets';
+	$viewRoot  = $_SERVER['DOCUMENT_ROOT'] . '/' . $assetRoot . '/views/';
 
 	//Routing
-	foreach ($router(null, '\\Controllers\\RootController', $request) as list($previous, $obj, $isEndpoint)) {
+	foreach ($router($context, '\\Controllers\\RootController', $request) as list($previous, $obj, $isEndpoint)) {
 		
 		if ($isEndpoint) { break; }
 
 	}
 
+	//Response preparation
+	$response = ['view' => 'noResource', 'context' => $context, 'assetRoot' => $assetRoot];
+
 	//Dispatch
-	$response = $dispatch($previous, $obj);
+	$response = array_merge($response, $dispatch($previous, $obj));
 
 	//Rendering
-	echo "Response: <br>" . var_export($response, true) . "<hr><br><hr>";
+	require_once 'vendor/Templating/phptenjin-0.0.2/lib/Tenjin.php';
+
+	$layoutFile = 'assets/views/master.phtml';
+	
+	if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
+		/* If it's an AJAX request, we can skip the layout file and simply render the individual view */
+		$layoutFile = null;
+	}
+
+	$properties = array('postfix'=>'.phtml', 'layout'=>$layoutFile, 'prefix'=>$viewRoot, 'cache'=>false, 'preprocess'=>true);
+
+	$engine = new \Tenjin_Engine($properties);
+
+	echo $engine->render(":" . $response['view'], $response);
 
 }
--- examples/Basic/vendor/Dispatchers/SimpleDispatcher.php
+++ examples/Basic/vendor/Dispatchers/SimpleDispatcher.php
@@ -5,18 +5,18 @@ namespace Dispatchers {
 
 		public function __invoke($previous, $obj) {
 
-			if (is_callable($obj)) { return $obj(); }
+			if (is_callable($obj)) { return [$obj()]; }
 
-			if (is_callable(array($obj, $previous))) { return $obj->$previous; }
+			if (is_callable(array($obj, $previous))) { return [$obj->$previous]; }
 
 			if (!is_object($obj)) { 
 
 				if (class_exists($obj)) { $obj = new $obj; } 
-				return var_export($obj, true); 
+				return [var_export($obj, true)]; 
 
 			}
 
-			return $obj;
+			return [$obj];
 		}
 
 	}
